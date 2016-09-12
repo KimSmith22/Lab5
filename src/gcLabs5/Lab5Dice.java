6ba948217ac4efbd569b78c01c2f13e9aa2a898f
Kimberli Smith <kimsmith.it@gmail.com>
2016-09-11 20:58:06 -0400

    initial commit

---
+++ src/gcLabs5/Lab5Dice.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package gcLabs5;
+
+/**
+ * @author Kimberli
+ *
+ */
+public class Lab5Dice {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
