87e7d39bfb17a85d59828128aaadb411d7a6a1a5
Kimberli Smith <kimsmith.it@gmail.com>
2016-09-12 16:41:48 -0400

    works with an error

--- src/gcLabs5/Lab5Dice.java
+++ src/gcLabs5/Lab5Dice.java
@@ -3,6 +3,9 @@
  */
 package gcLabs5;
 
+import java.util.Scanner;
+import java.util.Random;
+
 /**
  * @author Kimberli
  *
@@ -12,9 +15,34 @@ public class Lab5Dice {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static Scanner k;
 
+	public static final void main(String... aArgs){
+		  
+		int side;
+		String choice = "y";
+		
+	  System.out.println("Welcome!  Roll dice? y/n");
+		k = new Scanner(System.in);
+		choice = k.nextLine();
+		
+		do{
+			System.out.println("How many sides on your dice?: ");
+			side = k.nextInt();
+			System.out.println();
+			
+			Random randomGenerator = new Random();
+		    for (int i = 1; i <= 2; ++i){
+		      int randomInt = randomGenerator.nextInt(side);
+		      System.out.println(" " + randomInt);
+		      
+		}
+			System.out.println("Roll again?");
+			k.nextLine();
+			
+			choice = k.nextLine();	    
+		}while (choice.equals("y"));
+	  
 	}
-
+	
 }
