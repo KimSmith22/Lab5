47d79a43c93ede9abcbd1c15fdfdc166ac808309
Kimberli Smith <kimsmith.it@gmail.com>
2016-09-13 13:50:28 -0400

    final version; passed info using method

--- src/gcLabs5/Lab5Dice.java
+++ src/gcLabs5/Lab5Dice.java
@@ -17,32 +17,45 @@ public class Lab5Dice {
 	 */
 	private static Scanner k;
 
-	public static final void main(String... aArgs){
-		  
-		int side = 1;
+	public static void main(String [] args){
+		  		
 		String choice = "y";
+		int side;
 		
 	  System.out.println("Welcome!  Roll dice? y/n");
 		k = new Scanner(System.in);
 		choice = k.nextLine();
 		
-		do{
+		do{//begin do/while loop
 			System.out.println("How many sides on your dice?: ");
 			side = k.nextInt();
 			System.out.println();
 			
-			Random randomGenerator = new Random();
-		    for (int i = 1; i <= 2; ++i){
-		      int someInt = randomGenerator.nextInt(side);
-		      System.out.println(" " + someInt);
-		      
-		}
+			
+			//call method rollDice
+			Lab5Dice.rollDice(side);	//className.methodName(number of sides of dice); this is where you pass the info
+			
+		    //continue prompt
 			System.out.println("Roll again?");
 			k.nextLine();
 			
-			choice = k.nextLine();	    
+			choice = k.nextLine();	
+		//end do/while loop	
 		}while (choice.equals("y"));
+
+	}//psvm
 	  
-	}
+	//this is my rollDice method; it tells program what to do with dice and how to do it; this is the info to pass	
+	static void rollDice(int side){ //type output methodName(declare integer for number of sides of dice);
+		int a = (int)(Math.random()* side)+1; //using the Math.random method * number of sides + minimum number
+		int b = (int)(Math.random()* side)+1;
+		int c = a + b;
+		System.out.println("dice 1: "+ a);
+		System.out.println("dice 2: " +b);
+		System.out.println("total : " +c);
 	
-}
+
+		}//end rollDice Method	
+	}//end public class Lab5Dice
+	
+
